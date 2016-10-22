21efc194bc60029f7ba67475ea0bec892e397040
Sidharth Mishra <sidmishraw@gmail.com>
2016-10-22 01:15:49 -0700

    Initial Commit

---
+++ console/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  console
+//
+//  Created by Sidharth Mishra on 10/22/16.
+//  Copyright © 2016 Sidharth Mishra. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
